93f94027282e721bc788b22ea5135b2ac78df244
MNlegion <rkreuser30@gmail.com>
2024-01-01 22:19:43 -0500

    route and controller setup

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,35 @@
+// @desc   Get all items
+// @route GET /api/items
+// @access Private
+const getItems = (req, res) => {
+    res.status(200).json({ message: 'Get Items' })
+};
+
+// @desc   Create Item
+// @route POST /api/items
+// @access Private
+const createItem = (req, res) => {
+    res.status(200).json({ message: 'Create Item' })
+};
+
+// @desc   Update item by id
+// @route PUT /api/items/:id
+// @access Private
+const updateItem = (req, res) => {
+    res.status(200).json({ message: `Update Item ${req.params.id}` })
+};
+
+// @desc   Delete Item by id
+// @route DELETE api/items/:id
+// @access Private
+const deleteItem = (req, res) => {
+    res.status(200).json({ message: `Delete Item ${req.params.id}` })
+};
+
+
+module.exports = {
+    getItems,
+    createItem,
+    updateItem,
+    deleteItem
+}
--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { getItems, createItem, updateItem, deleteItem } = require('../controllers/itemController');
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get Items' })
-});
+router.get('/', getItems);
 
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Create Item' })
-});
+router.post('/', createItem);
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update Item ${req.params.id}` })
-});
+router.put('/:id', updateItem);
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Delete Item ${req.params.id}` })
-});
+router.delete('/:id', deleteItem);
 
 module.exports = router;
