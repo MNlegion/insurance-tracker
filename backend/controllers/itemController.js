52f74a9717a7e2b11467443b0db428576e0dde66
MNlegion <rkreuser30@gmail.com>
2024-01-02 13:47:37 -0500

    async handler function initialized

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc   Get all items
 // @route GET /api/items
 // @access Private
-const getItems = (req, res) => {
+const getItems = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get Items' })
-};
+});
 
 // @desc   Create Item
 // @route POST /api/items
 // @access Private
-const createItem = (req, res) => {
+const createItem = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please enter text')
     }
 
     res.status(200).json({ message: 'Create Item' })
-};
+});
 
 // @desc   Update item by id
 // @route PUT /api/items/:id
 // @access Private
-const updateItem = (req, res) => {
+const updateItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Item ${req.params.id}` })
-};
+});
 
 // @desc   Delete Item by id
 // @route DELETE api/items/:id
 // @access Private
-const deleteItem = (req, res) => {
+const deleteItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Item ${req.params.id}` })
-};
+});
 
 
 module.exports = {
