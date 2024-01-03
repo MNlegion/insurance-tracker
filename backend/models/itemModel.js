f88b5b7a8d655f89c65e2d8553a45037e3483ee8
MNlegion <rkreuser30@gmail.com>
2024-01-03 18:37:14 -0500

    item model revision and adjustment to controller as well to account for changes

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -5,7 +5,29 @@ const itemSchema = mongoose.Schema(
     text: {
       type: String,
       required: [true, "Please enter a description of the item."],
-    }
+    }, 
+    makeModelSerial: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Please enter a quantity."],
+    },
+    dateOfPurchase: {
+      type: Date,
+    },
+    costOfItem: {
+      type: Number,
+    },
+    costOfReplacement: {
+      type: Number,
+    },
+    itemCategory: {
+      type: String,
+    },
   },
   {
     timestamps: true,
