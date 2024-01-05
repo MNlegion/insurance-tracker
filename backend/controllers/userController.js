d565c92c68ebd6f2a209a02ec4eaab90ebe916a0
MNlegion <rkreuser30@gmail.com>
2024-01-04 19:56:08 -0500

    additional controllers for login and user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,27 @@
+// @desc   Register User
+// @route POST /api/users
+// @access Public
 const registerUser = (req, res) => {
     res.json({message: "Register User Successful"})
+}
+
+// @desc   Authenticate User
+// @route POST /api/users/login
+// @access Public
+const loginUser = (req, res) => {
+    res.json({message: "Login User Successful"})
+}
+
+// @desc   Get User Data
+// @route GET /api/users/me
+// @access Public
+const getMe = (req, res) => {
+    res.json({message: "Profile Data Retrieved"}) 
 }
 
 
 module.exports = { 
     registerUser,
+    loginUser,
+    getMe,
  };
