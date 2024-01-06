c000d38a1912c0cf245e1d041bd9a005da24b787
MNlegion <rkreuser30@gmail.com>
2024-01-06 17:53:08 -0500

    ensure user id is owner of item to update item

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,77 +1,92 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 
-const Item = require('../models/itemModel');
+const Item = require("../models/itemModel");
+const User = require("../models/userModel");
 
 // @desc   Get all items
 // @route GET /api/items
 // @access Private
 const getItems = asyncHandler(async (req, res) => {
-    const items = await Item.find();
+  const items = await Item.find({ user: req.user._id });
 
-    res.status(200).json(items)
+  res.status(200).json(items);
 });
 
 // @desc   Create Item
 // @route POST /api/items
 // @access Private
 const createItem = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
-        res.status(400)
-        throw new Error('Please enter text')
-    }
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please enter text");
+  }
 
-    const item = await Item.create({
-        text: req.body.text,
-        quantity: req.body.quantity,
-        // makeModelSerial: req.body.makeModelSerial,
-        // location: req.body.location,
-        // dateOfPurchase: req.body.dateOfPurchase,
-        // costOfItem: req.body.costOfItem,
-        // costOfReplacement: req.body.costOfReplacement,
-        // itemCategory: req.body.itemCategory,
-    })
+  const item = await Item.create({
+    text: req.body.text,
+    quantity: req.body.quantity,
+    user: req.user.id,
+    // makeModelSerial: req.body.makeModelSerial,
+    // location: req.body.location,
+    // dateOfPurchase: req.body.dateOfPurchase,
+    // costOfItem: req.body.costOfItem,
+    // costOfReplacement: req.body.costOfReplacement,
+    // itemCategory: req.body.itemCategory,
+  });
 
-    res.status(200).json(item)
+  res.status(200).json(item);
 });
 
 // @desc   Update item by id
 // @route PUT /api/items/:id
 // @access Private
 const updateItem = asyncHandler(async (req, res) => {
-    const item = await Item.findById(req.params.id);
+  const item = await Item.findById(req.params.id);
 
-    if(!item) {
-        res.status(404)
-        throw new Error('Item not found')
-    }
+  if (!item) {
+    res.status(404);
+    throw new Error("Item not found");
+  }
 
-    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    })
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json(updatedItem)
+  //   Check for User
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  //   Check if user owns item
+  if (item.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedItem);
 });
 
 // @desc   Delete Item by id
 // @route DELETE api/items/:id
 // @access Private
 const deleteItem = asyncHandler(async (req, res) => {
-    const item = await Item.findById(req.params.id);
+  const item = await Item.findById(req.params.id);
 
-    if(!item) {
-        res.status(404)
-        throw new Error('Item not found')
-    }
+  if (!item) {
+    res.status(404);
+    throw new Error("Item not found");
+  }
 
-    await item.deleteOne()
+  await item.deleteOne();
 
-    res.status(200).json({ id: req.params.id })
+  res.status(200).json({ id: req.params.id });
 });
 
-
 module.exports = {
-    getItems,
-    createItem,
-    updateItem,
-    deleteItem
-}
+  getItems,
+  createItem,
+  updateItem,
+  deleteItem,
+};
--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getItems, createItem, updateItem, deleteItem } = require('../controllers/itemController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(getItems).post(createItem);
-router.route('/:id').put(updateItem).delete(deleteItem);
+router.route('/').get(protect, getItems).post(protect, createItem);
+router.route('/:id').put(protect, updateItem).delete(protect, deleteItem);
 
 // Below individual routes are same as above but cleaner
 // router.get('/', getItems);
