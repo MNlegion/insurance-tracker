d017df64ff2cb165e313385f392144eb38cfddd9
MNlegion <rkreuser30@gmail.com>
2024-01-06 17:38:07 -0500

    getMe function to return req.user details upon endpoint hit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json(req.user)
+    const {_id, name, email} = await User.findById(req.user._id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
 // Generate JWT Token
