4a3c78ae42a2c435a0fa7ee7194167294b075180
MNlegion <rkreuser30@gmail.com>
2024-01-07 21:36:39 -0500

    initial register form setup

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,80 @@
-
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const { name, email, password, passwordConfirm } = formData;
+
+  const onChange = (e) => {};
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Create Account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Username"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="passwordConfirm"
+              name="passwordConfirm"
+              value={passwordConfirm}
+              placeholder="Confirm Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
